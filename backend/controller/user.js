e60bee4feeb4a8fd51a7e2d8f5676debf181d66d
Paul Sunny <98750088+Hexx00r@users.noreply.github.com>
2023-07-05 18:22:31 +0800

    Update user.js

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -45,7 +45,7 @@ router.post("/create-user", upload.single("file"), async (req, res, next) => {
 
     const activationToken = createActivationToken(user);
 
-    const activationUrl = `http://ecommerce-backend-s26.vercel.app/activation/${activationToken}`;
+    const activationUrl = `http://localhost:3000/activation/${activationToken}`;
 
     try {
       await sendMail({
