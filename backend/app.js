2d8c35fc86d1c82c392e86eb567a5df7081af217
Paul Sunny <98750088+Hexx00r@users.noreply.github.com>
2023-07-05 18:24:06 +0800

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const path = require("path");
 
 app.use(cors({
-  origin: ['https://ecommerce-backend-s26.vercel.app',],
+  origin: 'http://localhost:3000',
   credentials: true
 }));
 
